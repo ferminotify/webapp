0401ae9565c6f9bb41596b4e7e01c621a7e5d0b3
Kevin <kevliujudi@gmail.com>
2025-03-23 21:44:40 +0100

    db ssl cert conn

--- dbConfig.js
+++ dbConfig.js
@@ -7,7 +7,7 @@ const pool = new Pool({
   connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`,
   ssl: {
     rejectUnauthorized: true,
-    ca: Buffer.from(process.env.DB_SSL_CERT, 'base64').toString()
+    ca: process.env.DB_SSL_CERT
   }
 });
 
