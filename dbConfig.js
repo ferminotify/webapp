99c8a13216ac66eb8ea76a154423950dc307665d
Kevin <kevliujudi@gmail.com>
2025-05-08 13:49:06 +0200

    Fix db conn

--- dbConfig.js
+++ dbConfig.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 require("dotenv").config();
 
 const pool = new Pool({
-  connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`,
+  connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}?sslmode=require`,
   ssl: {
     rejectUnauthorized: true,
     ca: process.env.DB_SSL_CERT
